4cf8f7567f26cdda8903194977d97fe200dc08a9
valeriin0ry <153493936+valeriin0ry@users.noreply.github.com>
2025-04-08 14:04:23 +0300

    xampp

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -9,7 +9,7 @@ CREATE DATABASE ashurovLOGITpv23;
 USE ashurovLOGITpv23;
 
 --tabeli loomine
---identity(1,1) - ise täidab tabeli 1,2,3...
+--AUTO_INCREMENT - ise täidab tabeli 1,2,3...
 CREATE TABLE inimene(
 inimeneID int Primary Key identity(1,1),
 nimi varchar(50) unique,
@@ -28,12 +28,12 @@ DROP table inimene;
 --DML - data manipulation language
 
 INSERT INTO inimene
-(nimi, synniaeg, telefon, pikkus, opilaskodu, elukohtID, autoID)
+(nimi, synniaeg, telefon, pikkus, opilaskodu)
 VALUES
-('Peeter Uus', '2021-12-30', '2568952', 90.5, 0, 0),
-('Kaarel Oja', '2000-12-30', '2568952', 90.5, 0, 0),
-('Kask Mati', '1983-12-30', '33333', 140.5, 0, 0),
-('Peeter Oja', '2021-12-30', '333333', 190.5, 0, 0)
+('Peeter Uus', '2021-12-30', '2568952', 90.5, 0),
+('Kaarel Oja', '2000-12-30', '2568952', 90.5, 0),
+('Kask Mati', '1983-12-30', '33333', 140.5, 0),
+('Peeter Oja', '2021-12-30', '333333', 190.5, 0)
 ;
 SELECT * FROM inimene; 
 --kustuta id=2
