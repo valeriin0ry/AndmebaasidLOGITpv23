f2fe426a71df3dc4555c4b655a2e0da3c644ac5a
valeriin0ry <153493936+valeriin0ry@users.noreply.github.com>
2025-04-08 13:18:34 +0300

    Update

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -29,14 +29,87 @@ DROP table inimene;
 --DML - data manipulation language
 
 INSERT INTO inimene
-(nimi, synniaeg, telefon, pikkus, opilaskodu)
+(nimi, synniaeg, telefon, pikkus, opilaskodu, elukohtID, autoID)
 VALUES
-('Peeter Uus', '2021-12-30', '2568952', 90.5, 0),
-('Kaarel Oja', '2000-12-30', '2568952', 90.5, 0),
-('Kask Mati', '1983-12-30', '33333', 140.5, 0),
-('Peeter Oja', '2021-12-30', '333333', 190.5, 0)
+('Peeter Uus', '2021-12-30', '2568952', 90.5, 0, 0),
+('Kaarel Oja', '2000-12-30', '2568952', 90.5, 0, 0),
+('Kask Mati', '1983-12-30', '33333', 140.5, 0, 0),
+('Peeter Oja', '2021-12-30', '333333', 190.5, 0, 0)
 ;
 SELECT * FROM inimene; 
 --kustuta id=2
 DELETE FROM inimene
 WHERE inimeneID=2;
+
+
+--tabel elukoht
+CREATE TABLE elukoht(
+elukohtID int PRIMARY KEY identity (1,1),
+elukoht varchar(50) UNIQUE,
+maakond varchar(50)
+);
+SELECT * FROM elukoht;
+--andmete lisamine tabeli elukoht
+INSERT INTO elukoht(elukoht, maakond)
+VALUES ('Tallinn', 'Harjumaa'), 
+('Tartu', 'Tartumaa'),
+('Pärnu', 'Pärnumaa');
+
+--tabeli muutmine uue veergu lisamine
+ALTER TABLE inimene ADD elukohtID int;
+SELECT * FROM inimene;
+
+--foreign key lisamine
+
+ALTER TABLE inimene
+ADD constraint fk_elukoht
+FOREIGN KEY (elukohtID)
+references elukoht(elukohtID);
+
+SELECT * FROM inimene;
+SELECT * FROM elukoht;
+INSERT INTO inimene
+(nimi, synniaeg, telefon, pikkus, opilaskodu, elukohtID, autoID)
+VALUES
+('Peeter SUS', '2021-12-30', '2568952', 90.5, 0, 2, 1);
+INSERT INTO inimene
+(nimi, synniaeg, telefon, pikkus, opilaskodu, elukohtID, autoID)
+VALUES
+('Peeter SUSPECT', '2021-12-31', '3568952', 91.5, 0, 1, 1);
+SELECT * from inimene join elukoht
+ON inimene.elukohtID=elukoht.elukohtID;
+
+SELECT i.nimi, i.synniaeg, e.elukoht
+from inimene i join elukoht e
+ON i.elukohtID=e.elukohtID;
+
+--tabel auto
+CREATE TABLE auto(
+autoID int PRIMARY KEY identity(1,1),
+auto varchar(50) UNIQUE,
+autoNR varchar(12),
+mudell varchar(10),
+mark varchar(15),
+vaasta date,
+);
+
+INSERT INTO auto(auto, autoNR, mudell, mark, vaasta)
+VALUES ('2', 'EEE777', 'E34', 'BMW', '2017-12-30');
+
+
+SELECT * FROM auto
+
+SELECT * from inimene join auto 
+ON inimene.autoID=auto.autoID;
+
+
+SELECT i.nimi, i.synniaeg, a.auto
+from inimene i join auto a
+ON i.autoID=a.autoID;
+
+ALTER TABLE inimene ADD autoID int;
+
+ALTER TABLE inimene
+ADD constraint fk_auto
+FOREIGN KEY (autoID)
+references auto(autoID);
